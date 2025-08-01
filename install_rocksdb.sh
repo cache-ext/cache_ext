fdea469422164b7e5a695dfc54aa494de323f235
Tal Zussman <tz2294@columbia.edu>
2025-08-01 12:53:52 +0000

    Add kernel check to RocksDB scripts

--- install_rocksdb.sh
+++ install_rocksdb.sh
@@ -5,6 +5,12 @@ SCRIPT_PATH=$(realpath $0)
 BASE_DIR=$(dirname $SCRIPT_PATH)
 ROCKSDB_PATH="$BASE_DIR/rocksdb"
 
+if ! uname -r | grep -q "cache-ext"; then
+	echo "This script is intended to be run on a cache_ext kernel."
+	echo "Please switch to the cache_ext kernel and try again."
+	exit 1
+fi
+
 cd "$ROCKSDB_PATH"
 if [[ ! -e "CMakeLists.txt" ]]; then
 	git submodule update --init --recursive
